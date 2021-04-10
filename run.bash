560203d59c4c35840e8d91378f801dce56bbe73a
Paul Tarvydas <guitarvydas@gmail.com>
2021-04-10 07:13:00 -0400

    namedMonth

--- run.bash
+++ run.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
+cp big-test.gl test.gl
 ../grasem/run.bash ason.grasem <test.gl >_temp
 ../grasem/run.bash killws.grasem <_temp >_temp2
-../grasem/run.bash numerical.grasem <_temp2
-#cat _temp3
+../grasem/run.bash numerical.grasem <_temp2 >_temp3
+../grasem/run.bash date.grasem <_temp3 >_temp4
+cat _temp4
 
