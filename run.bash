7f7c5b6482ccaaf3ddfca94094faf40bc2012195
Paul Tarvydas <guitarvydas@gmail.com>
2021-04-16 13:00:43 -0400

    array/object/expression groups working by themselves
    adding defineWordGroup
    
    
    begin adding comment to eol
    
    
    comment in source
    
    
    date.grasem gobbles whitespace in text, fix
    
    
    expr identity in workflow
    
    
    G: and g:
    
    
    G: g: defineWord
    
    
    G g (no :) defineWord
    
    
    emitter identity for G g
    
    
    blocks as groups
    
    
    groups in lisp emitter
    
    
    negativenum in script before words
    
    
    fixed missing newline
    
    
    lisp emitter with negatives
    
    
    small-test revised - no tx: ; from: and statement: fields
    
    
    add balance: to small test
    
    
    define word takes 2 args
    
    
    add test.gl to .gitignore
    
    
    m4 include boilerplate
    
    
    -e flag
    
    
    defineWord in lisp emitter
    
    
    tighten check for e!
    
    
    remove incorrect design rule check
    
    
    defineWord in lisp emitter
    
    
    G consumes two words and nilgroup
    
    
    generic emitter with G 2xText

--- run.bash
+++ run.bash
@@ -1,12 +1,40 @@
 #!/bin/bash
-cp big-test.gl test.gl
-#cp small-test.gl test.gl
-../grasem/run.bash ason.grasem <test.gl >_temp
-../grasem/run.bash killws.grasem <_temp >_temp2
+set -e
+#cp big-test.gl test.gl
+cp small-test.gl test.gl
+echo 'ason'
+../grasem/run.bash ason.grasem <test.gl >_temp1
+echo 'killws'
+../grasem/run.bash killws.grasem <_temp1 >_temp2
+echo 'numerical'
 ../grasem/run.bash numerical.grasem <_temp2 >_temp3
+echo 'date'
 ../grasem/run.bash date.grasem <_temp3 >_temp4
+echo 'strings and binary'
 ../grasem/run.bash stringsAndBinary.grasem <_temp4 >_temp5
-../grasem/run.bash words.grasem <_temp5 >_temp6
-#../grasem/run.bash emitter.grasem <_temp6 >_temp7
-../grasem/run.bash emitter-lisp.grasem <_temp6 >_temp7
-cat _temp7
+echo 'negative numbers'
+../grasem/run.bash negativenum.grasem <_temp5 >_temp5a
+echo 'words'
+../grasem/run.bash words.grasem <_temp5a >_temp6
+echo 'expr'
+m4 expr.grasem > _.grasem
+../grasem/run.bash _.grasem <_temp6 >_temp7
+
+if `grep -q 'e!' _temp7`
+then
+  echo
+  echo '*** FAILURE: e! should have been removed by expr'
+  echo
+  exit 1
+fi
+
+
+
+echo 'generic emitter'
+m4 emitter.grasem >_.grasem
+
+# echo 'lisp emitter'
+# m4 emitter-lisp.grasem >_.grasem
+
+../grasem/run.bash _.grasem <_temp7 >_temp8
+
